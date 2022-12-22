68fc91f89c106e10d60f735026372214f9cec884
ripa6 <121214730+ripa6@users.noreply.github.com>
2022-12-22 11:44:54 +0600

    Add files via upload

---
+++ ripa 16/js/style.js
@@ -0,0 +1,25 @@
+
+(function($){
+	'use strict';
+
+//code start
+
+
+$('span i').on('click', function(){
+	$('.main-section-2'). fadeIn ();
+});
+
+
+$('.item-3 i').on ('click',function(){
+	$('.section-2'). fadeOut ();
+	$(this).hide();
+});
+
+
+
+
+
+
+
+
+}) (jQuery);
